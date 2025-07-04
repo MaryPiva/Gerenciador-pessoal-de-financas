67163f0d21dad509b08ea4218fe8da42b763835f
MaryPiva <mari.devpro@gmail.com>
2025-07-04 20:21:50 +0100

    .

--- PersonalFinanceManager/obj/Debug/net8.0/PersonalFinanceManager.AssemblyInfo.cs
+++ PersonalFinanceManager/obj/Debug/net8.0/PersonalFinanceManager.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PersonalFinanceManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+845becb3c8fcf7cf17d1a67c48e1cf01fcfc2ac9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+181f371c6b4317c84c9b4a48e5f8cd4f5e833084")]
 [assembly: System.Reflection.AssemblyProductAttribute("PersonalFinanceManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PersonalFinanceManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
